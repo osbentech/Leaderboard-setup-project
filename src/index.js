1d2beb86ead02e753ae4a8b7b9f25ac86ab4b203
Benjamin <benmma13@gmail.com>
2022-09-01 04:32:42 +0400

    fix linters, update js file

--- src/index.js
+++ src/index.js
@@ -3,43 +3,43 @@ import './main.css';
 const boardList = document.querySelector('.boardList');
 
 const boardListArr = [
-    {
-        name: 'John: 100',
-        Index: 0,
+  {
+    value: 'John: 100',
+    Index: 0,
+},
+{
+    value: 'Tom: 20',
+    Index: 1,
+},
+{
+    value: 'Mark: 50',
+    Index: 2,
+},
+{
+    value: 'Rita: 78',
+    Index: 3,
+},
+{
+    value: 'Nat: 125',
+    Index: 4,
+},
+{
+    value: 'Ben: 77',
+    Index: 5,
     },
     {
-        name: 'Tom: 20',
-        Index: 1,
-    },
-    {
-        name: 'Mark: 50',
-        Index: 2,
-    },
-    {
-        name: 'Rita: 78',
-        Index: 3,
-    },
-    {
-        name: 'Nat: 125',
-        Index: 4,
-    },
-    {
-        name: 'Ben: 77',
-        Index: 5,
-    },
-    {
-        name: 'Pat: 42',
-        Index: 6,
-    },
+    value: 'Pat: 42',
+    Index: 6,
+},
 ];
 
 function showScores() {
-    for (let i = 0; i < boardListArr.length; i += 1) {
-        boardList.innerHTML += ` <div class="score-board">
-        <p class="scores">${boardListArr[i].name}</p>
-      </div>
-        `
-    }
+  for (let i = 0; i < boardListArr.length; i += 1) {
+    boardList.innerHTML += ` <div class="score-board">
+    <p class="scores">${boardListArr[i].value}</p>
+    </div>
+        `;
+  }
 }
 
 showScores();
